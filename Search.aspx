390190a8162355befa08a0c27711fc1e468a8539
andratampu <tampu.andra@yahoo.ro>
2021-01-11 23:16:16 +0200

    added signup

---
+++ Search.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="Licenta.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Repeater ID="Repeater1" runat="server">
+            <HeaderTemplate>
+                <table cellspacing="0" rules="all" border="1">
+                    <tr>
+                        <th scope="col" style="width: 80px">
+                            ID
+                        </th>
+                        <th scope="col" style="width: 120px">
+                            Title
+                        </th>
+                    </tr>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblCustomerId" runat="server" Text='<%# Eval("ID") %>' />
+                    </td>
+                    <td>
+                        <asp:Label ID="lblContactName" runat="server" Text='<%# Eval("Title") %>' />
+                    </td>
+                </tr>
+            </ItemTemplate>
+            <FooterTemplate>
+                </table>
+            </FooterTemplate>
+        </asp:Repeater>
+    </form>
+</body>
+</html>
---
+++ Search.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace Licenta
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<Reciepe> reciepes = new List<Reciepe>(GetReceipes());
+            Repeater1.DataSource = reciepes;
+            Repeater1.DataBind();
+        }
+
+        string Baseurl = "https://api.spoonacular.com/";
+        public List<Reciepe> GetReceipes()
+        {
+            Root receipes = new Root();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.GetAsync("recipes/complexSearch?apiKey=4bf4ec1927934264b57d31947fcbb48e&query=pasta&number=3").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var receipeResponse = response.Content.ReadAsStringAsync().Result;
+                    receipes = JsonConvert.DeserializeObject<Root>(receipeResponse);
+
+                }
+                client.Dispose();
+                List<Reciepe> result = new List<Reciepe>(receipes.Results);
+
+                return result;
+            }
+        }
+
+
+    }
+}
